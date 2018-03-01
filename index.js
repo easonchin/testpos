75c6c975ce34b94a3fd714ccf10a9dbc3f689bb0
eason <estu4051@gmail.com>
2018-03-01 14:19:26 +0800

    test

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-# testpos
+var linebot = require('linebot');
+var express = require('express');
+
+var bot = linebot({
+  channelId: 1565315540,
+  channelSecret: 23ff6cd5f13d0a256616e02878d8f811,
+  channelAccessToken: mAlFPF/ZY5hhuJ5xlxtCf4A/XpEj6jCRuS7njLJjf+0yA7Sf6k9xRW0K//BPCsGhcKsxKIV6qitQHLMG5zNZIk4AS4u7m8isGeAf6FFyU/1BD+Qhv11WDqHynrAL/ereLJVH+YaXKSVc2OrzTxWxSAdB04t89/1O/w1cDnyilFU=
+});
+
