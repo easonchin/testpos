be9d3684ab955e1085041a15621ca85a6a08b590
eason <estu4051@gmail.com>
2018-03-01 14:21:10 +0800

    test01

--- index.js
+++ index.js
@@ -7,3 +7,17 @@ var bot = linebot({
   channelAccessToken: mAlFPF/ZY5hhuJ5xlxtCf4A/XpEj6jCRuS7njLJjf+0yA7Sf6k9xRW0K//BPCsGhcKsxKIV6qitQHLMG5zNZIk4AS4u7m8isGeAf6FFyU/1BD+Qhv11WDqHynrAL/ereLJVH+YaXKSVc2OrzTxWxSAdB04t89/1O/w1cDnyilFU=
 });
 
+
+bot.on('message', function(event) {
+  console.log(event); //把收到訊息的 event 印出來看看
+});
+
+const app = express();
+const linebotParser = bot.parser();
+app.post('/', linebotParser);
+
+//因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
+var server = app.listen(process.env.PORT || 8080, function() {
+  var port = server.address().port;
+  console.log("App now running on port", port);
+});
